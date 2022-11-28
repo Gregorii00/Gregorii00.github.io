61ada955ae21420c8c148bddea0062d089b3153a
Gregorii <107300093+Gregorii00@users.noreply.github.com>
2022-11-28 20:16:52 +0400

    Update index.js

--- assets/index.js
+++ assets/index.js
@@ -1,34 +1,8 @@
 function viewCanvas(number){
     let iA,iB, a, b, yExp, deltY;
-    let x = [-2,-1.5, 0, 0.5, 1, 1.25, 2, 3]; // Вводим x из таблицы
-    let y = [4.135335, 3.22313, 1, 0.648721, 0.718282, 0.990343, 3.389056, 14.08554]; // Вводим y из таблицы
+    let x = [-2,-1.5, 0, 0.5, 1, 1.25, 2, 3];
+    let y = [4.135335, 3.22313, 1, 0.648721, 0.718282, 0.990343, 3.389056, 14.08554]; 
     let y2 = [ ];
-    // let functions = {
-    //     1:Math.pow(x[i], 2)*2-3*x[i]+1, 
-    //     2:Math.pow(x[i], 2)*2-3*x[i]+1, 
-    //     3:(x[i]*2+1)*(x[i]-2)*(x[i]+3), 
-    //     4:(x[i]*2+1)*(x[i]-2)*(x[i]+3), 
-    //     5:Math.exp(x[i])-x[i], 
-    //     6:Math.sin(0.5*x[i])-x[i], 
-    //     7:Math.sin(0.5*x[i])-x[i], 
-    //     8:Math.exp(x[i])-2*x[i], 
-    //     9:(x[i]*2-1)*(2-x[i])*(x[i]+3), 
-    //     10:(x[i]*2+1)*(x[i]-2)*(x[i]+3), 
-    //     11:Math.pow(x[i], 2)*2-3*x[i]+1, 
-    //     12:Math.pow(x[i], 2)*2-3*x[i]+1, 
-    //     13:Math.pow(x[i], 2)*2-3*x[i]+1, 
-    //     14:Math.pow(x[i], 2)*2-3*x[i]+1, 
-    //     15:Math.pow(x[i], 2)-4*x[i]+5, 
-    //     16:Math.pow(x[i], 2)+4*x[i]-5, 
-    //     17:Math.pow(x[i], 2)-4*x[i]+5, 
-    //     18:Math.pow(x[i], 2)-4*x[i]+5, 
-    //     19:Math.pow(x[i], 2)+4*x[i]-5,
-    //     20:(x[i]*2+1)*(x[i]+1)*(x[i]-3), 
-    //     21:(x[i]*2+1)*(x[i]+1)*(x[i]-3), 
-    //     22:(x[i]*2+3)*(x[i]-2)*(x[i]+1), 
-    //     23:(x[i]*2+3)*(x[i]-2)*(x[i]+1), 
-    //     24:(x[i]*2+5)*(x[i]-3)*(x[i]-1)
-    // };
     for(i=1;i<x.length;i++){
         if(number>= x[i-1] &&number<= x[i]){
             iA=i-1;
@@ -40,12 +14,12 @@ function viewCanvas(number){
     yExp = number*a+b;
     
     for (i=0;i<x.length;i++){
-        y2[i]=Math.exp(x[i])-2*x[i]; // Здесь прописываем формулу которая у вас (y2[i]= не меняем)
+        y2[i]=Math.exp(x[i])-2*x[i];
     }
     if(yExp == y[iA] || yExp == y[iB]){
         deltY = 0;
     } else{
-        deltY =  Math.abs(Math.exp(number)-2*number)/Math.abs(yExp); // Здесь прописываем формулу которая у вас number вместо x ставим ( Math.abs(Math.exp(number)-2*number) )
+        deltY =  Math.abs(Math.exp(number)-2*number)/Math.abs(yExp);
     }
 
     let ctx = document.getElementById('myChart').getContext('2d');
